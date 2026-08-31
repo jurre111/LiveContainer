8a425f7e64d5c79dc8609e2f889406bb8c7229a6
jurre111 <jurredijkstra@mijnheem.nl>
2026-09-01 01:01:48 +0200

    ig

--- MultitaskSupport/ResizeHandleView.m
+++ MultitaskSupport/ResizeHandleView.m
@@ -5,6 +5,7 @@
 //  Created by Duy Tran on 2/6/25.
 //
 #import "ResizeHandleView.h"
+#import <UIKit/UIKit.h>
 
 @implementation ResizeHandleView
 - (instancetype)initWithFrame:(CGRect)frame {
@@ -12,12 +13,12 @@
     self.backgroundColor = [UIColor clearColor];
     self.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
 
-    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(0,0) radius:25.0 startAngle:9.0*M_PI/180.0 endAngle:81.0*M_PI/180.0 clockwise:YES];
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(0,0) radius:20.0 startAngle:9.0*M_PI/180.0 endAngle:81.0*M_PI/180.0 clockwise:YES];
     CAShapeLayer *layer = [CAShapeLayer layer];
     layer.path = path.CGPath;
     layer.strokeColor = [UIColor labelColor].CGColor;
     layer.fillColor = [UIColor clearColor].CGColor;
-    layer.lineWidth = 2.0;
+    layer.lineWidth = 3.0;
     layer.lineCap = kCALayerLineCapRound;
     [self.layer addSublayer:layer];
     return self;
