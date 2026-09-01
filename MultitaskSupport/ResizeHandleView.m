3506800a67701bae274546d3da29552ce7aa3525
jurre111 <jurredijkstra@mijnheem.nl>
2026-09-01 18:00:55 +0200

    math

--- MultitaskSupport/ResizeHandleView.m
+++ MultitaskSupport/ResizeHandleView.m
@@ -15,12 +15,12 @@
     self.backgroundColor = [UIColor clearColor];
     self.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
 
-    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(0,0) radius:30 startAngle:9.0*M_PI/180.0 endAngle:81.0*M_PI/180.0 clockwise:YES];
+    UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(10,10) radius:23 startAngle:9.0*M_PI/180.0 endAngle:81.0*M_PI/180.0 clockwise:YES];
     CAShapeLayer *layer = [CAShapeLayer layer];
     layer.path = path.CGPath;
     layer.strokeColor = [UIColor labelColor].CGColor;
     layer.fillColor = [UIColor clearColor].CGColor;
-    layer.lineWidth = 3.0;
+    layer.lineWidth = 4.0;
     layer.lineCap = kCALineCapRound;
     [self.layer addSublayer:layer];
     return self;
