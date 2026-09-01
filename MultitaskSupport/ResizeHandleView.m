1024bbbd50975b8d8a98d622ce921e590c707fe4
jurre111 <jurredijkstra@mijnheem.nl>
2026-09-01 18:23:22 +0200

    hitbox

--- MultitaskSupport/ResizeHandleView.m
+++ MultitaskSupport/ResizeHandleView.m
@@ -7,13 +7,17 @@
 #import "ResizeHandleView.h"
 #import <UIKit/UIKit.h>
 #import <QuartzCore/QuartzCore.h>
-#import <math.h>
 
 @implementation ResizeHandleView
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
-    self.backgroundColor = [UIColor clearColor];
+    self.clipsToBounds = YES;
     self.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin;
+    UIView *backgroundView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, frame.size.width*sqrt(2), frame.size.height*sqrt(2))];
+    backgroundView.backgroundColor = [UIColor colorWithWhite:1 alpha:0.01];
+    backgroundView.center = CGPointMake(frame.size.width, frame.size.height);
+    backgroundView.transform = CGAffineTransformMakeRotation(M_PI_4);
+    [self addSubview:backgroundView];
 
     UIBezierPath *path = [UIBezierPath bezierPathWithArcCenter:CGPointMake(10,10) radius:23 startAngle:9.0*M_PI/180.0 endAngle:81.0*M_PI/180.0 clockwise:YES];
     CAShapeLayer *layer = [CAShapeLayer layer];
